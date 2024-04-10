e35e018257d7b20563d888d88d7e3e41090707cd
Jacob Jumper <JacobJumper@gmail.com>
2024-04-09 21:58:31 -0500

    Login Persistence(Ammend)

---
+++ DatingApp/client/src/app/_services/account.service.ts
@@ -0,0 +1,40 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, map } from 'rxjs';
+import { User } from '../_models/user';
+
+// Handles the HTTP Requests from Client to Server
+
+//Angular services are initialized when the app is, and aren't destroyed until the app is
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AccountService {
+  baseUrl = 'http://localhost:5001/api/';
+  private currentUserSource = new BehaviorSubject<User | null>(null);
+  currentUser$ = this.currentUserSource.asObservable();
+
+  constructor(private http: HttpClient) { }
+
+  login(model: any){
+    return this.http.post<User>(this.baseUrl + 'account/login', model).pipe(
+      map((response: User) => {
+        const user = response;
+        if (user){
+          localStorage.setItem('user', JSON.stringify(user))
+          this.currentUserSource.next(user);
+        }
+      })
+    )
+  }
+
+  setCurrentUser(user : User){
+    this.currentUserSource.next(user);
+  }
+
+  logout(){
+    localStorage.removeItem('user')
+    this.currentUserSource.next(null);
+  }
+}
