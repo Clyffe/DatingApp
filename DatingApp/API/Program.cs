e2d20f83a3613e7e751858b5fc428ce2d3b93bdc
Jacob Jumper <JacobJumper@gmail.com>
2025-12-03 17:17:52 -0600

    Added Middleware For Testing

--- DatingApp/API/Program.cs
+++ DatingApp/API/Program.cs
@@ -1,6 +1,7 @@
 using System.Text;
 using API.Data;
 using API.Interfaces;
+using API.Middleware;
 using API.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
@@ -35,6 +36,7 @@ var app = builder.Build();
 
 
 //Configure the HTTP Request Pipeline
+app.UseMiddleware<ExceptionMiddleware>();
 app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
 app.UseAuthentication();
